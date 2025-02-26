05b06d0e0e6d3067c32d8dea3aed8469c0ab0db9
waldev <walter.villamarr@ug.edu.ec>
2025-02-26 01:49:29 -0500

    jasper

--- src/pasantias/Main.java
+++ src/pasantias/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
         try {
             // Conexión a la base de datos
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3308/prueba", "root", "040902");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/prueba", "root", "04090202");
 
             // Extraer detalles del rol
             RolGeneral rol = ExtractDetails.getRolByEmpleado(connection, empCodigo);
